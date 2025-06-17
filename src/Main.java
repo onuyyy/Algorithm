84fcdd0c5d80e78192dc894ee0dac7acba4e5b61
onuyyy <qazxsw9026@naver.com>
2025-06-17 17:34:15 +0900

    star

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import static star.Star.*;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        System.out.println("method 1");
+        method1();
+        System.out.println("method 2");
+        method2();
+        System.out.println("method 3");
+        method3();
+        System.out.println("method 4");
+        method4();
+        System.out.println("method 5");
+        method5();
+        System.out.println("method 6");
+        method6();
+        System.out.println("method 7");
+        method7();
     }
 }
---
+++ src/star/Star.java
@@ -0,0 +1,93 @@
+package star;
+
+public class Star {
+    public static void method1() {
+        int star = 5;
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j < star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void method2() {
+        int star = 0;
+        while (star <= 5) {
+            for (int i = 0; i < 5; i++) {
+                if (star == i) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            star++;
+            System.out.println();
+        }
+    }
+
+    public static void method3() {
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
+
+    public static void method4() {
+        int left = 0;
+        int right = 5;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (left == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            left++;
+            for (int j = 5; j >= right; j--) {
+                if (right == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            right--;
+            System.out.println();
+        }
+    }
+
+    public static void method5() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void method6() {
+        int star = 0;
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 5; j > i - 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void method7() {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*****");
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
