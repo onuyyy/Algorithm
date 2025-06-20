ca41a9dbd46999b41735a8489575b2b05e6ac4b5
onuyyy <qazxsw9026@naver.com>
2025-06-20 15:45:23 +0900

    백준 0620 오늘도 풀었당

--- src/문자열/BJ_1316.java
+++ src/문자열/BJ_1316.java
@@ -22,16 +22,16 @@ public class BJ_1316 {
         br.close();
     }
 
-    public static boolean check(String str) {
-        int prev = 0;   // 이저 문자 아스키 코드 저장
+    private static boolean check(String str) {
+        int prev = -1;   // 이전 문자 아스키 코드 저장
         boolean[] check = new boolean[26];  // 알파벳 사용 여부 기록
 
         for (int i = 0; i < str.length(); i++) {
-            int now = str.charAt(i);    // 현재 문자 아스키 코드
+            int now = str.charAt(i) - 'a'; // a 안 빼주면 s 첫글자의 아스키 코드가 그대로 나옴
 
             if (prev != now) {
-                if (!check[now - 'a']) {
-                    check[now - 'a'] = true;
+                if (!check[now]) {
+                    check[now] = true;
                     prev = now;
                 } else {
                     return false;   // 이미 나온 문자인데 연속하지 않을 때 : 그룹 문자 X
